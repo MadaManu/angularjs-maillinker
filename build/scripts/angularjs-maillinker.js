1d23338530ef5187613c30608401299b78bc7d46
MadaManu <madalindruta92@gmail.com>
2015-07-23 17:24:48 +0100

    testing data service and new build

--- build/scripts/angularjs-maillinker.js
+++ build/scripts/angularjs-maillinker.js
@@ -1,24 +1,14 @@
 'use strict';
 angular.module('maillinker', []);
-(function() {
-  'use strict';
-  var DataService = function($q) {
+angular.module('maillinker')
+  .factory('DataService', ['$q',function ($q) {
     return {
-      getProviders : function() {
-        // $http.get('/api/people/pikachu').success(function(data) {
-        //   $scope.name = data.name;
-        // }).error(function(data) {
-        //   $scope.name = data;
-        // });
+      getProviders: function () {
         void 0;
         return true;
       }
-    }
-  };
-  DataService.$inject = ['$q'];
-
-  angular.module('maillinker').service('DataService', DataService);
-})();
+    };
+}]);
 angular.module('maillinker')
   .factory('MailLinkerService', ['$q','$rootScope','$window',function ($q,$rootScope,$window) {
     return {
--- services/DataService.js
+++ services/DataService.js
@@ -1,19 +1,9 @@
-(function() {
-  'use strict';
-  var DataService = function($q) {
+angular.module('maillinker')
+  .factory('DataService', ['$q',function ($q) {
     return {
-      getProviders : function() {
-        // $http.get('/api/people/pikachu').success(function(data) {
-        //   $scope.name = data.name;
-        // }).error(function(data) {
-        //   $scope.name = data;
-        // });
-        console.log("FUNCTION CALL! A");
+      getProviders: function () {
+        console.log("get providers!!!");
         return true;
       }
-    }
-  };
-  DataService.$inject = ['$q'];
-
-  angular.module('maillinker').service('DataService', DataService);
-})();
+    };
+}]);
